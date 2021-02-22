ed70f14b21c771224fc471b48192c365e0e28c03
abdounovich <abdounovich@gmail.com>
2021-02-22 13:26:27 +0100

    comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -119,7 +119,7 @@ body {
   <body>
 
     <div id="loading" style="width: 100%; height:100%"  class="bg-white">
-      <img id="loading-image" src="https://cdn.dribbble.com/users/579645/screenshots/2951106/basic_loading_loop.gif" alt="Loading..." />
+      <img id="loading-image" width="50px" height="50px" src="https://cdn.dribbble.com/users/579645/screenshots/2951106/basic_loading_loop.gif" alt="Loading..." />
       </div> 
    <script type="application/javascript">
 
